54d77faac80da01b39b98a8dd88c76790f931e06
rachitkakkar <rachitkakkar7@gmail.com>
2023-09-07 17:51:33 -0500

    Add periodic execution (10ms)

--- src/main/java/PurpleVision.java
+++ src/main/java/PurpleVision.java
@@ -1,10 +1,22 @@
 import java.io.IOException;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Main Class
  */
 public class PurpleVision {
+    VisionSystem visionSystem;
+
+    private static void run() { // Temp function for now, eventually will replace with the visionSystem run function
+        System.out.println("Running: " + new java.util.Date());
+    }
+
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello, World!");
+        ScheduledExecutorService executorService;
+        executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(PurpleVision::run, 0, 20, TimeUnit.MILLISECONDS); // Run every 10ms
     }
 }
